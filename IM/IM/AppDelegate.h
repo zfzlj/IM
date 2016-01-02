4c17812d5fd830f3770d5bbd5adaf3c2bb7c97a2
zfzlj <zhaofeizlj@163.com>
2016-01-02 13:42:16 +0800

    初始化项目

---
+++ IM/IM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IM
+//
+//  Created by mac on 16/1/2.
+//  Copyright © 2016年 ZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
